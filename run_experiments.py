ac4503dca215555ec3ee79aad284fbdafd58651d
Carlos Henrique <carloshkayser@gmail.com>
2022-10-31 18:43:58 +0000

    Add grid search script

--- run_experiments.py
+++ run_experiments.py
@@ -1,10 +1,12 @@
 # Importing Python libraries
+from subprocess import Popen, DEVNULL
 import itertools
 import time
 import os
 
-SYNC_EXECUTION = True
-
+SYNC_EXECUTION = False
+NUMBER_OF_PARALLEL_PROCESSES = os.cpu_count()
+# NUMBER_OF_PARALLEL_PROCESSES = 10
 
 def run_simulation(dataset: str, algorithm: str, n_gen: int, pop_size: int, cross_prob: float, mut_prob: float):
     """Executes the simulation with the specified parameters.
@@ -18,22 +20,20 @@ def run_simulation(dataset: str, algorithm: str, n_gen: int, pop_size: int, cros
         mut_prob (float): NSGA-II's mutation probability.
     """
     # Running the simulation based on the parameters and gathering its execution time
-    cmd = f"nohup python3 -B -m simulation -d {dataset} -a {algorithm} -p {pop_size} -g {n_gen} -c {cross_prob} -m {mut_prob} &"
-    print(f"    cmd = {cmd}")
+    cmd = f"python3 -B -m simulation -d {dataset} -a {algorithm} -p {pop_size} -g {n_gen} -c {cross_prob} -m {mut_prob}"
+    # print(f"    cmd = {cmd}")
 
-    stream = os.popen(cmd)
-    if SYNC_EXECUTION:
-        stream.read()
+    return Popen(cmd.split(" "), stdout=DEVNULL, stderr=DEVNULL)
 
 
 # Parameters
 datasets = ["datasets/dataset1.json"]
 algorithms = ["nsgaii"]
 
-population_sizes = [300]
-number_of_generations = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
-crossover_probabilities = [1]
-mutation_probabilities = [0, 0.2, 0.4, 0.6, 0.8, 1]
+population_sizes = [100, 200, 300]
+number_of_generations = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1500, 2000, 2500, 3000]
+crossover_probabilities = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+mutation_probabilities = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
 
 # Generating list of combinations with the parameters specified
 combinations = list(
@@ -46,6 +46,8 @@ combinations = list(
         mutation_probabilities,
     )
 )
+
+processes = []
 
 # Executing simulations and collecting results
 print(f"EXECUTING {len(combinations)} COMBINATIONS")
@@ -62,7 +64,7 @@ for i, parameters in enumerate(combinations, 1):
     print(f"\t\t[{algorithm}] dataset={dataset}. pop_size={pop_size}. n_gen={n_gen}. cross_prob={cross_prob}. mut_prob={mut_prob}")
 
     # Executing algorithm
-    run_simulation(
+    proc = run_simulation(
         dataset=dataset,
         algorithm=algorithm,
         pop_size=pop_size,
@@ -70,3 +72,13 @@ for i, parameters in enumerate(combinations, 1):
         cross_prob=cross_prob,
         mut_prob=mut_prob,
     )
+
+    processes.append(proc)
+
+    while len(processes) > NUMBER_OF_PARALLEL_PROCESSES:
+        proc = processes[0]
+        proc.wait()
+        processes.pop(0)
+        print(f"PID {proc.pid} finished")
+
+    print(f"{len(processes)} processes running in parallel")
